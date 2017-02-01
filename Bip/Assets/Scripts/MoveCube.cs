6c74c7376a86a5fa5409b225fff968e407c04525
Logan <logan.porati@gmail.com>
2017-02-01 16:33:05 +0100

    Les trucs qui tombent

--- Bip/Assets/Scripts/MoveCube.cs
+++ Bip/Assets/Scripts/MoveCube.cs
@@ -6,128 +6,22 @@ public class MoveCube : MonoBehaviour {
 
     public float speed = 5;
 
-    public float range = 10;
-
-    public enum Actions
-    {
-        Nothing,
-        Flee,
-        Chase
-    }
-
-    public Actions action;
-
-    public GameObject target;
 
     private Rigidbody rb;
 
-    private float timer = 0;
-
-    public float timerMax = 1;
-
+  
 	// Use this for initialization
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
-        GetTarget();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        timer += Time.deltaTime;
-        if (action == Actions.Nothing || timer >= timerMax)
-        {
-            timer = 0;
-            GetTarget();
-        }
-
-
-        if (action == Actions.Chase)
-        {
-            rb.MovePosition(Vector3.MoveTowards(rb.position, target.transform.position, speed * Time.deltaTime));
-        }
-
-        else if (action == Actions.Flee)
-        {
-            rb.MovePosition(Vector3.MoveTowards(rb.position, (transform.position - target.transform.position) * 1000, speed * Time.deltaTime));
-        }
-
-        rb.velocity = Vector3.zero;
+       
 		
 	}
-
-    void GetTarget()
-    {
-        Collider[] surroundings = Physics.OverlapSphere(transform.position, range);
-
-        target = null;
-        float targetDist = float.MaxValue;
-
-        foreach (Collider col in surroundings)
-        {
-            if (col.gameObject != gameObject && Vector3.Distance(transform.position, col.transform.position) < targetDist && col.tag != tag)
-            {
-                targetDist = Vector3.Distance(transform.position, col.transform.position);
-                target = col.gameObject;
-            }
-        }
-
-
-        action = Actions.Nothing;
-
-        if (target)
-        {
-            switch (target.tag)
-            {
-                case "Aa":
-                    switch (tag)
-                    {
-                        case "Bb":
-                            action = Actions.Flee;
-                            break;
-
-                        case "Cc":
-                            action = Actions.Chase;
-                            break;
-
-                    }
-
-                    break;
-
-                case "Bb":
-                    switch (tag)
-                    {
-                        case "Aa":
-                            action = Actions.Chase;
-                            break;
-
-                        case "Cc":
-                            action = Actions.Flee;
-                            break;
-
-                    }
-
-                    break;
-
-                case "Cc":
-                    switch (tag)
-                    {
-                        case "Aa":
-                            action = Actions.Flee;
-                            break;
-
-                        case "Bb":
-                            action = Actions.Chase;
-                            break;
-
-                    }
-
-                    break;
-
-            }
-        }
-    }
 
     private void OnDrawGizmos()
     {
---
+++ Bip/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+
+    public GameObject[] prefabs;
+
+
+    public float timerSpawn = 2;
+
+    private float timer = 0;
+
+    public int numberToSpawn = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+
+        if (timer >= timerSpawn)
+        {
+            timer -= timerSpawn;
+
+            for (int i = 0; i < numberToSpawn; i++)
+            {
+                int rand = Random.Range(0, prefabs.Length);
+
+                for (int j = 0; j < 200; j++)
+                {
+
+                    Vector3 newPos = new Vector3(Random.Range(Camera.main.ScreenToWorldPoint(Vector3.zero).x, Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0, 0)).x), transform.position.y);
+
+                    if (Physics.OverlapSphere(newPos, prefabs[rand].GetComponent<SphereCollider>().radius).Length == 0)
+                    {
+                        GameObject newEntity = Instantiate(prefabs[rand]);
+
+                        newEntity.transform.position = newPos;
+
+                        break;
+                    }
+
+                }
+
+                
+
+            }
+
+        }
+		
+	}
+}
