a239362e3cdf6c7bacfa76844ba0740d6a7faf74
PSTER1S0005\Etudiant <john.dublin@voila.fr>
2017-02-08 17:12:05 +0100

    Variation sur le taille des agents (random encadré)

---
+++ Bip/Assets/Scripts/IterationTaille.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IterationTaille : MonoBehaviour {
+
+	public float tailleMin;
+	public float tailleMax;
+	float TAILLE;
+
+
+	// Use this for initialization
+	void Start () {
+		TAILLE = Random.Range (tailleMin, tailleMax);
+		transform.localScale = new Vector3 (TAILLE, TAILLE, TAILLE);
+	}
+}
