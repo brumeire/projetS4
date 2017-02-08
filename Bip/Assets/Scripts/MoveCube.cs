f7037b90dffc1974e66efcb8d979b1f64db7ccba
Logan <logan.porati@gmail.com>
2017-02-08 17:33:27 +0100

    Slow speed iteration

--- Bip/Assets/Scripts/MoveCube.cs
+++ Bip/Assets/Scripts/MoveCube.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveCube : MonoBehaviour
 {
+    public float speed = 100;
     ConstantForce constantForce;
     // Use this for initialization
     void Start()
@@ -11,17 +12,19 @@ public class MoveCube : MonoBehaviour
         constantForce = GetComponent<ConstantForce>();
         //GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0) * 0.1f);
         transform.localEulerAngles = new Vector3(0, 0, Random.Range(0f, 360f));
+
+        constantForce.relativeForce = new Vector3(0, speed, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (constantForce.relativeForce.y > 100)
+        if (constantForce.relativeForce.y > speed)
             constantForce.relativeForce -= new Vector3(0, 0.2f * Time.deltaTime, 0);
 
-        if (constantForce.relativeForce.y < 100)
-            constantForce.relativeForce = new Vector3(0, 100, 0);
+        if (constantForce.relativeForce.y < speed)
+            constantForce.relativeForce = new Vector3(0, speed, 0);
 
         else if (constantForce.relativeForce.y > 500)
             constantForce.relativeForce = new Vector3(0, 500, 0);
